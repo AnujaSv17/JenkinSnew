42c61067d9812d6365e73074e3678b0a2bbed57c
git-AnujaSv17 <anuja.sv@lntinfotech.com>
2021-03-22 15:38:34 +0530

    Changed text

--- JenkinSnew/Program.cs
+++ JenkinSnew/Program.cs
@@ -11,6 +11,7 @@ namespace JenkinSnew
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+            Console.WriteLine("Anuja");
         }
     }
 }
